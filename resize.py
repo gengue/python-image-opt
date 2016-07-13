1b5c968d23f95ec33a84d35b33c024cf8bfeadd5
Genesis Guerrero Martinez <genesisdaft@gmail.com>
2016-07-13 09:55:07 -0500

    check argv

--- resize.py
+++ resize.py
@@ -17,8 +17,9 @@ size = 800, 500
 path = '.'
 newpath = 'processed'
 
-if sys.argv[1] and sys.argv[2]:
-    size = int(sys.argv[1]), int(sys.argv[2])
+if len(sys.argv) > 1:
+    if sys.argv[1] and sys.argv[2]:
+        size = int(sys.argv[1]), int(sys.argv[2])
 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
@@ -27,6 +28,7 @@ for infile in os.listdir(path):
     current = os.path.join(path, infile)
 
     if (os.path.isfile(current) and (infile.endswith('.jpg') or infile.endswith('.png') or  infile.endswith('.gif'))):
+        print('processing "%s"' % infile)
         img = Image.open(infile)
         img.thumbnail(size, Image.ANTIALIAS)
         img.save('processed/%s' % infile, optimize=True, quality=95)
